c1b43d50ad2b58543cee7142a8101781f7d9e3af
iandol <iandol@aristotle>
2018-07-08 20:51:04 +0800

    1.2.5 — becite/betitle/beall first AND last author names with initials are now shown, and if an attachment is present you can Quicklook it directly from Alfred without losing focus (press shift or ⌘Y)!

--- source/findReferencesAll.rb
+++ source/findReferencesAll.rb
@@ -1,18 +1,15 @@
 #!/usr/bin/env ruby
+
 require 'json'
-#require 'byebug/core'
-#require 'byebug'
-#PORT = 8989
-#Byebug.wait_connection = true
-#Byebug.start_server('127.0.0.1', PORT)
 #======class definition======
 class FindReferencesAll
-	attr_accessor :version, :using_alfred
-	VER = '1.0.5'.freeze
+	attr_accessor :version, :using_alfred, :attachments_folder
+	VER = '1.0.7'.freeze
 	#--------------------- constructor
-	def initialize # set up class
+	def initialize
 		@version = VER
 		@using_alfred = false
+		@attachments_folder = ''
 		@cansearch = false
 		@comment = ''
 		@raw = ''
@@ -33,16 +30,16 @@ class FindReferencesAll
 		input = input[0].split(' ') if input.length == 1
 		@raw = input
 		input.each do |my_input|
-			if my_input.match(/^-?\d{1,4}$/)
+			if my_input =~ /^-?\d{1,4}$/
 				@year = my_input.to_s
-			elsif my_input.match(/^\S+$/)
+			elsif my_input =~ /^\S+$/
 				@names.push(my_input)
 			end
 		end
 		@cansearch = true unless @names.empty?
 	end
 
-	def constructSQL()
+	def constructSQL
 		return unless @cansearch
 		@names.each do |name|
 			if @SQL.empty?
@@ -51,14 +48,12 @@ class FindReferencesAll
 				@SQL += " AND (allFields REGEX '(?i)#{name}')"
 			end
 		end
-		unless @year.empty?
-			@SQL = "(" + @SQL + ") AND thedate REGEX '#{@year}'"
-		end
+		@SQL = '(' + @SQL + ") AND thedate REGEX '#{@year}'" unless @year.empty?
 	end
 
-	def doSQLSearch()
+	def doSQLSearch
 		return unless @cansearch
-		rec = osascript <<-EOT
+		rec = osascript <<-APPL
 		tell application "Bookends"
 			set mynull to ASCII character 30
 			set results to «event RubySQLS» "#{@SQL}"
@@ -68,28 +63,27 @@ class FindReferencesAll
 			end try
 			return results & mynull & BEversion
 		end tell
-		EOT
+		APPL
 		rec = rec.split("\u001E")
 		@list = rec[0].chomp.split("\r")
 		@BEVersion = 13 if rec[1].match(/^13/)
 	end
 
-	def getRecords()
+	def getRecords
 		return unless @cansearch
 		return if @list.empty?
-		mylist = @list.join(",")
-		rec = osascript <<-EOT
+		mylist = @list.join(',')
+		rec = osascript <<-APPL
 		tell application "Bookends"
 			return «event RubyRJSN» "#{mylist}" given string:"title,authors,thedate,attachments,user1"
 		end tell
-		EOT
+		APPL
 		rec = JSON.parse(rec)
 		rec.each_with_index do |thisrec, i|
 			@title[i] = thisrec['title'].chomp.strip
 			@title[i] = 'Blank' if @title[i].nil? || @title[i].empty?
 
-			@authors[i] = thisrec['authors'].chomp.strip.split(',')[0]
-			@authors[i] = 'Unknown' if @authors[i].nil? || @authors[i].empty?
+			@authors[i] = parseAuthors(thisrec['authors'])
 
 			@date[i] = thisrec['thedate'].chomp.strip.split(/[\s\/-]/)[0]
 			@date[i] = '????' if @date[i].nil? || @date[i].empty?
@@ -98,19 +92,19 @@ class FindReferencesAll
 			@uuid[i] = '-1' if @uuid[i].nil? || @uuid[i].empty?
 
 			@attachments[i] = thisrec['attachments'].to_s.chomp.strip
-			@attachments[i] = '📎' unless @attachments[i].empty?
+			@attachments[i] = ENV['attachmentsFolder'] + @attachments[i] unless @attachments[i].empty?
 
 			@key[i] = thisrec['user1'].to_s.chomp.strip
 			@key[i] = '' if @uuid[i].nil? || @uuid[i].empty?
 		end
 	end
 
-	def getRecordsLegacy()
+	def getRecordsLegacy
 		return unless @cansearch
 		return if @list.empty?
-		mylist = @list.join(",")
-		myorder = ['title','authors','date','uniqueid']
-		rec = osascript <<-EOT
+		mylist = @list.join(',')
+		myorder = ['title', 'authors', 'date', 'uniqueid']
+		rec = osascript <<-APPL
 		tell application "Bookends"
 			set mynull to ASCII character 30
 			set ti to «event RubyRFLD» "#{mylist}" given string:"title"
@@ -119,7 +113,7 @@ class FindReferencesAll
 			set uid to «event RubyRFLD» "#{mylist}" given string:"uniqueID"
 			return ti & mynull & au & mynull & da & mynull & uid
 		end tell
-		EOT
+		APPL
 		rec = rec.split("\u001E")
 		rec.each_with_index do |thisrec, i|
 			thisrec = thisrec.split("\u0000")
@@ -146,7 +140,7 @@ class FindReferencesAll
 					uid = '0' if uid.to_s.empty?
 					@uuid[j] = uid.chomp.strip
 				end
-			end		
+			end
 		end
 	end
 
@@ -157,41 +151,85 @@ class FindReferencesAll
 		jsonin = []
 		@uuid.each_with_index do |uuid, i|
 			icon = 'file.png'
-			#icon = 'file+attachment.png' unless @attachments[i].empty?
-			jsonin[i] = {
-				uid: uuid,
-				arg: uuid,
-				title: @authors[i] + ' ' + @date[i] + '   ' + @attachments[i],
-				subtitle: @title[i],
-				icon: {path: "#{icon}"}
-			}
+			# icon = 'file+attachment.png' unless @attachments[i].empty?
+			if @attachments[i].empty?
+				jsonin[i] = {
+					uid: uuid,
+					arg: uuid,
+					title: @authors[i] + '  (' + @date[i] + ')',
+					subtitle: @title[i],
+					icon: { path: icon.to_s }
+				}
+			else
+				jsonin[i] = {
+					uid: uuid,
+					arg: uuid,
+					title: @authors[i] + '  (' + @date[i] + ')  📎',
+					quicklookurl: @attachments[i],
+					subtitle: @title[i],
+					icon: { path: icon.to_s }
+				}
+			end
 		end
-		jsono= { comment: "NAMES=#{@names.join(' & ')} | YEAR=#{@year} | SQL=#{@SQL}",
+		jsono = {
+			comment: "NAMES=#{@names.join(' & ')} | YEAR=#{@year} | SQL=#{@SQL}",
 			items: jsonin,
-			length: jsonin.length }
+			length: jsonin.length
+		}
 		puts JSON.generate(jsono)
 	end
 
 	def returnNullResults
 		if @using_alfred
-			jsono = { comment: "No Results!", 
-			items: [ ],
-			length: 0 }
+			jsono = {
+				comment: 'No Results!',
+				items: [],
+				length: 0
+			}
 			puts JSON.generate(jsono)
 		else
 			puts 'No results found!'
 		end
 	end
 
-	def doSearch
-		self.constructSQL
-		self.doSQLSearch
-		if @BEVersion == 13
-			self.getRecords
+	#=== set up et al., etc based on author numbers
+	def parseAuthors(myInput)
+		authors = myInput.chomp.strip.split("\n")
+		if authors.length == 1
+			return processAuthor(authors[0])
+		elsif authors.length == 2
+			return processAuthor(authors[0]) + ' & ' + processAuthor(authors[1])
 		else
-			self.getRecordsLegacy
+			return processAuthor(authors[0]) + '  …  ' + processAuthor(authors[-1])
 		end
-		self.returnResults
+	end
+
+	def processAuthor(authorName)
+		familyName = 'Unknown'
+		initial = ''
+		unless authorName.nil?
+			s = authorName.split(',')
+			if s.length == 1
+				familyName = s[0].chomp.strip
+			elsif s.length == 2
+				familyName = s[0].chomp.strip
+				initial = ' ' + s[1].gsub!(/^\s*(\w{1})(.*)/, '\1').chomp.strip
+			else
+				familyName = 'Unknown'
+			end
+		end
+		return familyName + initial
+	end
+
+	def doSearch
+		constructSQL
+		doSQLSearch
+		if @BEVersion == 13
+			getRecords
+		else
+			getRecordsLegacy
+		end
+		returnResults
 	end
 
 	# this converts to -e line format so osascript can run, pass in a heredoc
@@ -200,13 +238,15 @@ class FindReferencesAll
 		cmd = ['osascript'] + script.split(/\n/).map { |line| ['-e', line] }.flatten
 		IO.popen(cmd) { |io| return io.read }
 	end
-end #====== end Class ======
+end
+#====== end Class ======
 
 #=== Create object and run it ===
-fR = FindReferencesAll.new
-
+fR = FindReferences.new
 # check if running under alfred
 fR.using_alfred = true unless ENV['alfred_version'].nil?
+# check if we were passed the attachment folder path
+fR.attachments_folder = ENV['attachmentsFolder'] unless ENV['attachmentsFolder'].nil?
 
 if ARGV.nil?
 	fR.returnNullResults
